150c8bf906d9e5cc3de46752d1028cd114e1974d
Danny Kirchmeier <danny@kirchmeier.us>
2018-12-03 11:37:22 -0600

    Day 02

---
+++ 02/main.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from "fs";
+import _ = require("lodash");
+import { expect } from 'chai';
+
+function main() {
+  const inputFile = readFileSync(__dirname + "/input.txt", 'utf8').trim();
+  const exampleA = "abcdef bababc abbcde abcccd aabcdd abcdee ababab";
+
+  expect(solveA(exampleA)).to.equal(12);
+  console.log("A:", solveA(inputFile));
+
+  const exampleB = "abcde fghij klmno pqrst fguij axcye wvxyz";
+
+  expect(solveB(exampleB)).to.equal("fgij");
+  console.log("B:", solveB(inputFile));
+}
+
+main();
+
+function solveA(input: string) {
+  let words = input.split(/\s/);
+  let twice = 0;
+  let thrice = 0;
+  for (let w of words) {
+    let counts = _.values(_.countBy(w.split('')));
+    if (counts.includes(2)) twice++;
+    if (counts.includes(3)) thrice++;
+  }
+  return twice * thrice;
+}
+
+function solveB(input: string) {
+  let words = input.split(/\s/);
+
+  for(let i = 0; i < words.length; i++){
+    let wi = words[i].split('');
+
+    for(let j = i + 1; j < words.length; j++){
+      let wj = words[j].split('');
+
+      let common = '';
+      for(let n = 0; n < wj.length; n++){
+        if(wi[n] === wj[n]) common += wi[n];
+      }
+
+      if(common.length === wi.length - 1) return common;
+    }
+  }
+
+  return -1;
+}
